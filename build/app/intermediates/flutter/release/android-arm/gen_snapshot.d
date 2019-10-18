639bbced023a7b6b65070a2f7eadf953128f06c8
stefan <stefan@du.digitalgit config --global user.name stefangit config --global user.email stefan@du.digitalgit config --global user.email stefan@du.digital>
2019-10-18 16:24:37 +0200

    LATEST BUILD

---
+++ build/app/intermediates/flutter/release/android-arm/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Users/admin/Desktop/Flutter/flutter/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
---
+++ build/app/intermediates/flutter/release/android-arm64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Users/admin/Desktop/Flutter/flutter/bin/cache/artifacts/engine/android-arm64-release/darwin-x64/gen_snapshot
